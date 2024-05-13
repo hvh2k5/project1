b981e2cf226fc5d8c86b70fd310b291370d4e9d7
ngo-duy <166359669+ngo-duy@users.noreply.github.com>
2024-05-13 13:38:29 +0700

    Delete project1/admin directory

--- project1/admin/auth/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if(isset($_POST['submit'])){
-    unset( $_SESSION['auth']['admin']);
-    unset( $_SESSION['auth']['email']);
-
-}
-header("Location:/project1/admin/auth/login.php");
-
