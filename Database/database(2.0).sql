bede52134133742dcfcb7a1d13a68e63cf72f643
hvh2k5 <165681036+hvh2k5@users.noreply.github.com>
2024-05-22 13:43:46 +0700

    Update database(2.0).sql

--- Database/database(2.0).sql
+++ Database/database(2.0).sql
@@ -15,7 +15,8 @@ CREATE TABLE admins (
 
 -- Tạo bảng customers
 CREATE TABLE customers (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEYKEY,
+    name VARCHAR(200) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(50) NOT NULL,
     phone INT(10) NOT NULL UNIQUE,
